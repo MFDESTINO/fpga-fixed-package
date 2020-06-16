232308f5c17c62b966d2243704636661d3b0f5e4
couto0 <daniel.couto64@gmail.com>
2020-06-16 12:10:03 -0300

    alguns ajustes

--- fixed_package.vhd
+++ fixed_package.vhd
@@ -5,10 +5,10 @@ USE IEEE.numeric_std.all;
 package fixed_package is
 	constant	MAX_IND	: integer := 15;
 	constant MIN_IND : integer := -15;
-		
+	
 	subtype	fixed_range	is	integer range MAX_IND downto MIN_IND;
 	type fixed is array (integer range <>) of bit;
-	
+		
 	function to_fixed (arg: integer; 
 	          max_range: fixed_range := MAX_IND; 
 				 min_range: fixed_range := 0) return fixed; 
@@ -32,14 +32,14 @@ package body fixed_package is
 	variable i   : integer := min_range;
 	variable fix : fixed (max_range downto min_range);
 	variable int : integer := arg * 2 ** (-(min_range));
-	variable len : integer := max_range + (-(min_range));
-	variable sig : std_logic_vector (len downto 0) := std_logic_vector(to_signed(int, len+1));
-	
-	begin
-			assert(len < MAX_IND + 1) report "out of range" severity error;
+	variable len : integer := max_range + (-(min_range)) + 1;
+	variable sig : std_logic_vector (len - 1 downto 0) := std_logic_vector(to_signed(int, len));
 			
+	begin
+			assert(len <= MAX_IND + 1) report "out of range" severity error;
+
 			escreve_bit_a_bit: while (i <= max_range) loop
-			fix(i) := to_bit(sig(i+(-min_range)));
+			fix(i) := to_bit(sig(i+(-min_range))); --faz um offset no indice de sig
 			i := i+1;
 			end loop;
 			
@@ -55,14 +55,14 @@ package body fixed_package is
 	variable i   : integer := min_range;
 	variable fix : fixed (max_range downto min_range);
 	variable int : integer := integer(arg * real(2 ** (-(min_range))));
-	variable len : integer := max_range + (-(min_range));
-	variable sig : std_logic_vector (len downto 0) := std_logic_vector(to_signed(int, len+1));
+	variable len : integer := max_range + (-(min_range)) + 1;
+	variable sig : std_logic_vector (len - 1 downto 0) := std_logic_vector(to_signed(int, len));
 	
 	begin
-			assert(len < MAX_IND + 1) report "out of range" severity error;
+			assert(len <= MAX_IND + 1) report "out of range" severity error;
 			
 			escreve_bit_a_bit: while (i <= max_range) loop
-			fix(i) := to_bit(sig(i+(-min_range)));
+			fix(i) := to_bit(sig(i+(-min_range))); --faz um offset no indice de sig
 			i := i+1;
 			end loop;
 			
@@ -77,13 +77,13 @@ package body fixed_package is
 	variable min_range : integer := arg'low;
 	variable i         : integer := min_range;
 	variable int       : integer := 0;
-	variable len       : integer := max_range + (-(min_range));
-	variable sig       : std_logic_vector (len downto 0);
+	variable len       : integer := max_range + (-(min_range)) + 1;
+	variable sig       : std_logic_vector (len - 1 downto 0);
 	
 	begin
 			escreve_bit_a_bit: while (i <= max_range) loop
-			sig(i+(-min_range)) := to_stdulogic(arg(i));
-			i := i+1;
+			sig(i+(-min_range)) := to_stdulogic(arg(i)); --faz um offset no indice de sig
+			i := i+1;			
 			end loop;
 			int := to_integer(signed(sig)) / 2 ** (-(min_range));
 			return int;
@@ -97,12 +97,12 @@ package body fixed_package is
 	variable min_range : integer := arg'low;
 	variable i         : integer := min_range;
 	variable r         : real;
-	variable len       : integer := max_range + (-(min_range));
-	variable sig       : std_logic_vector (len downto 0);
+	variable len       : integer := max_range + (-(min_range)) + 1;
+	variable sig       : std_logic_vector (len - 1 downto 0);
 	
 	begin
 			escreve_bit_a_bit: while (i <= max_range) loop
-			sig(i+(-min_range)) := to_stdulogic(arg(i));
+			sig(i+(-min_range)) := to_stdulogic(arg(i)); --faz um offset no indice de sig
 			i := i+1;
 			end loop;
 			r := real(to_integer(signed(sig))) / real(2 ** (-(min_range)));
